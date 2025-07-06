b2132891ab6108fecd22389072306ef72555c5ab
Selezionexyz <selezionexyzweb@gmail.com>
2025-07-07 01:58:42 +0200

    Create ComparateurLuxe.jsx

---
+++ src/components/ComparateurLuxe.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ComparateurLuxe() {
+  return (
+    <div style={{ padding: "2rem", color: "white" }}>
+      <h1>NOM_DU_COMPOSANT</h1>
+      <p>Contenu à venir...</p>
+    </div>
+  );
+}
