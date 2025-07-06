674a848555128246d0174c95cc1d99a3937daeb7
Selezionexyz <selezionexyzweb@gmail.com>
2025-07-07 01:53:01 +0200

    Create FicheProduit.jsx

---
+++ src/components/FicheProduit.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FicheProduit() {
+  return (
+    <div style={{ padding: "2rem", color: "white" }}>
+      <h1>Fiche Produit</h1>
+      <p>Contenu à venir...</p>
+    </div>
+  );
+}
