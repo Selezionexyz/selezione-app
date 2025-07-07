cd4b9937d75a183550c01d43f0c6c5c8f0e8036f
Selezionexyz <selezionexyzweb@gmail.com>
2025-07-07 17:13:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-// App.jsx
 import React from "react";
 import SaasLayout from "./components/SaasLayout";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
-  return <SaasLayout />;
+  return (
+    <ErrorBoundary>
+      <div className="min-h-screen bg-black text-white">
+        <SaasLayout />
+      </div>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
