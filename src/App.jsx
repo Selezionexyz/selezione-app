00ae4bda030a21f7f10e32238858773c86e01f07
Selezionexyz <selezionexyzweb@gmail.com>
2025-07-07 16:23:53 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import SaasLayout from "./components/SaasLayout";
-import Navbar from "./components/Navbar";
-import Dashboard from "./components/Dashboard";
-import AssistantLuxe from "./components/AssistantLuxe";
-import ComparateurLuxe from "./components/ComparateurLuxe";
-import EstimationLuxe from "./components/EstimationLuxe";
-import FicheProduit from "./components/FicheProduit";
-import ScraperVC from "./components/ScraperVC";
-import Quiz from "./components/Quiz";
-import Academy from "./components/Academy";
+import AppContent from "./components/AppContent"; // Si tu veux séparer encore mieux plus tard
 
 function App() {
-  const [currentComponent, setCurrentComponent] = useState("dashboard");
-
-  const renderComponent = () => {
-    switch (currentComponent) {
-      case "dashboard": return <Dashboard />;
-      case "assistant": return <AssistantLuxe />;
-      case "fiche": return <FicheProduit />;
-      case "estimation": return <EstimationLuxe />;
-      case "comparateur": return <ComparateurLuxe />;
-      case "scraper": return <ScraperVC />;
-      case "quiz": return <Quiz />;
-      case "academy": return <Academy />;
-      default: return <Dashboard />;
-    }
-  };
-
-  return (
-    <SaasLayout>
-      <Navbar setCurrentComponent={setCurrentComponent} />
-      {renderComponent()}
-    </SaasLayout>
-  );
+  return <SaasLayout />;
 }
 
 export default App;
