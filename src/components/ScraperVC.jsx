070387f51a55e76912dadc1157a3fdee92859cb7
Selezionexyz <selezionexyzweb@gmail.com>
2025-07-07 02:00:03 +0200

    Create ScraperVC.jsx

---
+++ src/components/ScraperVC.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ScraperVC() {
+  return (
+    <div style={{ padding: "2rem", color: "white" }}>
+      <h1>NOM_DU_COMPOSANT</h1>
+      <p>Contenu à venir...</p>
+    </div>
+  );
+}
