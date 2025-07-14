ae95c9f20df8eb07f170ee0ac7e5b28999645a52
Selezionexyz <selezionexyzweb@gmail.com>
2025-07-14 20:42:07 +0200

    Update Input.jsx

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,11 +1,10 @@
-// /src/components/ui/Input.jsx
 import React from 'react';
 
 export function Input({ className = '', ...props }) {
   return (
     <input
       type="text"
-      className={`text-black bg-white p-3 rounded w-full ${className}`}
+      className={`p-3 rounded w-full ${className}`}
       {...props}
     />
   );
